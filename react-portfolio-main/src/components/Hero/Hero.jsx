beafbe550de9eaed35a6e743b2c6032a4c4270dc
Varsha Singh <80753426+VarshaSingh-11@users.noreply.github.com>
2024-03-28 00:51:20 +0530

    initial

---
+++ react-portfolio-main/src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Physio Health Plus</h1>
+        <p className={styles.description}>
+        Best Physiotherapy Clinic in Gurgaon, well-known Physiotherapy Rehabilitation center founded by Dr. Amit Kumar (Neuro physiotherapist, Osteopath, certified Chiropractor) in 2005
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/hero2.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
